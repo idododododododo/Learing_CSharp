66c9e8813c6c465e0edfc0a132ff2d5d5dd4d2fc
idododododododo <124135985+idododododododo@users.noreply.github.com>
2023-05-02 13:24:50 +0300

    project origin

---
+++ myFIrstConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace myFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SideA;
+            //inslzing var
+            double SideB;
+            //inslzing var
+            double sidec;
+            //inslzing var
+            Console.WriteLine("enter side a ");
+            //writing to user  what input  do i want to get
+            SideA=  Convert.ToDouble(Console.ReadLine());
+            //getting SideA from user input, and converting it to double 
+            Console.WriteLine("enter side b");
+            //writing to user  what input  do i want to get
+
+            SideB=  Convert.ToDouble(Console.ReadLine());
+            //getting SideA from user input, and converting it to double 
+
+            SideA = Math.Pow(SideA, 2);
+            //getting the var power 2 
+            SideB = Math.Pow(SideB, 2);
+            //getting the var power 2
+            sidec = Math.Sqrt(SideA + SideB);
+            //
+            Console.WriteLine(sidec);
+
+
+
+
+        }
+    }
+}
